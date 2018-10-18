07bcf8f283bb0bc9b5651125751970dfe856323c
Jana Eggink <jana.eggink@bbc.co.uk>
2018-10-18 10:36:25 +0100

    for move to bbc-datalab project on gcs secrets are not deployed, so this will only run locally

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ REQUEST_TIMEOUT = 5
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", DEFAULT_LOG_LEVEL)
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-GCP_PROJECT_NAME = 'bbc-connected-data'
+GCP_PROJECT_NAME = 'bbc-datalab'
 DATA_DUMP_FILE_NAME = os.getenv("DATA_DUMP_FILE_NAME", "scv_data_sample.json")
 
-PSEUDOCONE_GCS_BUCKET = 'pseudocone_data_dump'
+PSEUDOCONE_GCS_BUCKET = 'pseudocone_data_dump_v2'
